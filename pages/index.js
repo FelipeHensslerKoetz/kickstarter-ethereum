c46e0de20381771a74e80aba53c834ae8b5a56fa
Felipe Henssler Koetz <felipehenssler@gmail.com>
2023-11-26 15:29:59 -0300

    chore: add h3

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ class CampaignIndex extends Component {
     return (
       <div>
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"></link>
+        <h3>Open Campaigns</h3>
         {this.renderCampaigns()}
         <Button
           content="Create Campaign"
