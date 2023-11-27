b7a55075beff1ab195d6dd2c3205ac2ffd336ea1
Felipe Henssler Koetz <felipehenssler@gmail.com>
2023-11-27 12:50:49 -0300

    chore: form error message and spinner

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -9,20 +9,52 @@ class RequestNew extends Component {
   state = {
     value: '',
     description: '',
-    recipient: ''
+    recipient: '',
+    loading: false,
+    errorMessage: ''
   };
 
   static async getInitialProps(props) {
     const { address } = props.query;
 
     return { address };
-  }
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+
+    const campaign = Campaign(this.props.address);
+    const { description, value, recipient } = this.state;
+
+    this.setState({ loading: true, errorMessage: '' });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      await campaign.methods.createRequest(
+        description,
+        web3.utils.toWei(value, 'ether'),
+        recipient
+      ).send({ from: accounts[0] });
+
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+    } catch (error) {
+      this.setState({ errorMessage: error.message });
+    }
+
+    this.setState({ loading: false });
+  };
 
   render() {
     return (
       <Layout>
+        <Link route={`/campaigns/${this.props.address}/requests`}>
+          <a>
+            Voltar
+          </a>
+        </Link>
         <h3>Criar uma nova requisição</h3>
-        <Form>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Descrição</label>
             <Input
@@ -46,11 +78,12 @@ class RequestNew extends Component {
               onChange={event => this.setState({ recipient: event.target.value })}
             />
           </Form.Field>
-          <Button primary>Criar</Button>
+          <Message error header="Oops!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>Criar</Button>
         </Form>
       </Layout>
     );
-  }
+  };
 }
 
 export default RequestNew;
