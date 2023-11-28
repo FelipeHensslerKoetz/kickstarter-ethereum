ae92a8b479319434ba2a93c2b5a59ea004fc18e3
Felipe Henssler Koetz <felipehenssler@gmail.com>
2023-11-27 21:39:06 -0300

    chore: fix approvers count and invalid request value

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaigFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaigFactory.interface),
-  '0x947085223a1a7b66152c14C182860fbec7A8c296'
+  '0xf47A1aE5246FE350fea6B6503999f4847e763fA9'
 );
 
 export default instance;
